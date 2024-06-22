60fa7c505cd730b063fedf9365b878c1c13ee040
theminacious <zacksunrise32@gmail.com>
2024-06-23 03:23:01 +0530

    "Added Scroll component to Home page and updated description of Overnight Delivery in cardData.json"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from '@/components/HeroSection'
 import OurFeatures from '../components/OurFeatures'
+import Scroll from '@/components/scroll';
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
    <HeroSection/>
    <OurFeatures/>
+   <Scroll/>
     </main>
   );
 }
---
+++ src/components/Scroll.tsx
@@ -0,0 +1,70 @@
+
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
+
+const deliveryContent = [
+  {
+    title: "Scheduled Deliveries",
+    description:
+      "Schedule deliveries at your convenience. Choose the date and time that works best for you, ensuring your packages arrive precisely when needed.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--purple-500),var(--indigo-500))] flex items-center justify-center text-white">
+        Scheduled Deliveries
+      </div>
+    ),
+  },
+  {
+    title: "Special Handling",
+    description:
+      "Require special handling for fragile items? We offer specialized packaging and handling to ensure delicate shipments reach their destination intact.",
+    content: (
+        <div className="h-full w-full  flex items-center justify-center text-white">
+        <Image
+          src="https://cdn.pixabay.com/photo/2016/07/27/15/56/link-1545625_960_720.jpg"
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Express Shipping",
+    description:
+      "Need urgent delivery? Opt for our express shipping service for swift and expedited delivery of your packages, guaranteeing fast turnaround times.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--red-500),var(--orange-500))] flex items-center justify-center text-white">
+        Express Shipping
+      </div>
+    ),
+  },
+  {
+    title: "Customized Solutions",
+    description:
+      "Tailored delivery solutions to meet your unique needs. Whether it's bulk shipments, specialized routes, or specific delivery instructions, we've got you covered.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--teal-500),var(--cyan-500))] flex items-center justify-center text-white">
+        Customized Solutions
+      </div>
+    ),
+  },
+];
+
+
+
+
+
+
+const Scroll = () => {
+  return (
+    <div>
+        <StickyScroll content={deliveryContent} />
+      
+    </div>
+  )
+}
+
+
+
+export default Scroll
